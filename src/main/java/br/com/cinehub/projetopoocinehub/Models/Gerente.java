c4af948ca79c711cec1336f79d322126b2a68e08
TaysaGuiral <taysaguiral2@gmail.com>
2024-11-01 18:07:23 -0300

    Remove Cliente and Remove Manager

--- src/main/java/br/com/cinehub/projetopoocinehub/Models/Gerente.java
+++ src/main/java/br/com/cinehub/projetopoocinehub/Models/Gerente.java
@@ -8,8 +8,13 @@ public class Gerente extends Usuario {
     void cadastroGerente(Cadastro cadastro, String email, String senha, String nome) {
         cadastro.cadastroGerente(email, senha, nome);
     }
+    //Remover o acesso de um usuario
     void removerUsuario(Cadastro cadastro,String email) {
         cadastro.removeCliente(email);
+    }
+    //Remover o acesso de um gerente
+    void removerGerente(Cadastro cadastro,String email) {
+        cadastro.removeGerente(email);
     }
     void realizarCompra(Filme filme){}
     void apagarComentario(Comentarios comentario){}
