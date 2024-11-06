3176ffb2993f0418cc13a191009647d768f3b367
Anna Beatriz Souza <122402769+annab3atriz@users.noreply.github.com>
2024-11-06 08:57:52 -0300

    Update Main.java

--- src/main/java/br/com/cinehub/projetopoocinehub/Models/Main.java
+++ src/main/java/br/com/cinehub/projetopoocinehub/Models/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         Cadastro cadastro = new Cadastro();
-        MenuInicial.Menu(sc, cadastro);
+        Menu.menuInicial(sc, cadastro);
         sc.close();
     }
 
