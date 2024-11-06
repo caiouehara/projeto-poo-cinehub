fe09121e059a016664fc97dd355c62aa7f05c598
Anna Beatriz Souza <122402769+annab3atriz@users.noreply.github.com>
2024-11-06 08:51:08 -0300

    Add files via upload

---
+++ src/main/java/br/com/cinehub/projetopoocinehub/Models/TesteFilme.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+public class TesteFilme {
+  
+    public static void main(String args[]) {
+        ArrayList<Filme> catalogoFilmes = new ArrayList<Filme>();
+        Filme shrek = new Filme(2001, "Shrek", "saasa", 7.0, 120.0, 30.0, 7.0, 7,0,"assas");
+        catalogoFilmes.add(shrek);
+        Filme shrek2 = new Filme(2003, "Shrek 2", "saasa", 7.0, 120.0, 30.0, 7.0, 7,0,"assas");
+        catalogoFilmes.add(shrek2);
+        Filme shrek3 = new Filme(2006, "Shrek 3", "saasa", 7.0, 120.0, 30.0, 7.0, 7,0,"assas");
+        catalogoFilmes.add(shrek3);
+        //Menu.filmeMenu(catalogoFilmes);
+        Cliente anna = new Cliente("anna", "anna", "anna");
+        anna.realizarCompra(shrek);
+        anna.realizarAluguel(shrek2);
+        anna.realizarAluguel(shrek3);
+        anna.getCompra().imprimirFilmesComprados();
+        anna.getAluguel().imprimirFilmesAlugados();
+        System.out.println("Gasto total aluguel: " + anna.getGastoAluguel());
+        System.out.println("Gasto total compra: " + anna.getGastoCompra());
+
+        
+    }
+}
+
+//adicionar filme no catálogo (x)
+//imprimir catálogo (x) --> mudar para toString()
+
