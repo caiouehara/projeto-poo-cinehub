eb7311357e89390d9a0520e473aa824afd2a04ba
Anna Beatriz Souza <122402769+annab3atriz@users.noreply.github.com>
2024-11-30 15:35:45 -0300

    Javadoc do Usuario.java

--- src/main/java/br/com/cinehub/projetopoocinehub/Models/User/Usuario.java
+++ src/main/java/br/com/cinehub/projetopoocinehub/Models/User/Usuario.java
@@ -1,4 +1,18 @@
-// Usuario.java
+/**
+ * Classe abstrata que representa um usuário do sistema.
+ * Essa classe utiliza anotações do Jackson para definir a serialização e
+ * desserialização baseadas no tipo de usuário.
+ *
+ * <p>Os tipos de usuários podem ser:
+ * <ul>
+ *   <li>{@link Cliente}</li>
+ *   <li>{@link Gerente}</li>
+ * </ul>
+ *
+ * @see Cliente
+ * @see Gerente
+ */
+
 package br.com.cinehub.projetopoocinehub.Models.User;
 
 import br.com.cinehub.projetopoocinehub.Models.User.Tipos.Cliente;
@@ -18,15 +32,30 @@ import com.fasterxml.jackson.annotation.JsonProperty;
     @JsonSubTypes.Type(value = Gerente.class, name = "Gerente")
 })
 public abstract class Usuario {
+      /** Nome do usuário. */
     protected String nome;
+    /** E-mail do usuário. */
     protected String email;
+    /** Senha do usuário. */
     protected String senha;
+    /** Tipo de usuário (ex.: "Cliente", "Gerente"). */
     protected String tipoDeUsuario;
 
+    /**
+     * Construtor padrão.
+     */
     // Construtores
     public Usuario() {
     }
-
+    
+    /**
+     * Construtor que inicializa os atributos do usuário.
+     *
+     * @param nome          o nome do usuário.
+     * @param email         o e-mail do usuário.
+     * @param senha         a senha do usuário.
+     * @param tipoDeUsuario o tipo de usuário.
+     */
     public Usuario(String nome, String email, String senha, String tipoDeUsuario) {
         this.nome = nome;
         this.email = email;
@@ -35,21 +64,38 @@ public abstract class Usuario {
     }
 
     // Getters e Setters
+    /**
+     * Obtém o nome do usuário.
+     *
+     * @return o nome do usuário.
+     */
     @JsonProperty("nome")
     public String getNome() {
         return nome;
     }
-
+    /**
+     * Obtém o e-mail do usuário.
+     *
+     * @return o e-mail do usuário.
+     */
     @JsonProperty("email")
     public String getEmail() {
         return email;
     }
-
+    /**
+     * Obtém a senha do usuário.
+     *
+     * @return a senha do usuário.
+     */
     @JsonProperty("senha")
     public String getSenha() {
         return senha;
     }
-
+     /**
+     * Obtém o tipo de usuário.
+     *
+     * @return o tipo de usuário.
+     */
     @JsonProperty("tipoDeUsuario")
     public String getTipoDeUsuario() {
         return tipoDeUsuario;
